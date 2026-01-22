d9810ae728a0a4a4b0fa23ab40aa67980ae18869
ajay0600 <ajaypalsrn@gmail.com>
2026-01-22 13:03:23 +0530

    restapi

---
+++ restapis/user-service/src/main/java/com/example/demo/entities/Users.java
@@ -0,0 +1,38 @@
+package com.example.demo.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="user")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Users {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	int user_id;
+	String user_name;
+	String password;
+	String email;
+	String contact_no;
+	String permanent_address;
+	String current_address;
+	String first_name;
+	String last_name;
+	String aadhar;
+	String aadhar_img_path;
+	int role_id;
+	
+	
+	
+}
